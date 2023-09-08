5baf9651250fe478a62cc77ce33ad63f1aaf8d79
Vincent Tse <river_run@126.com>
2023-09-08 12:49:16 +0800

    fix: remove umd format when building bg script

--- vite.background.config.ts
+++ vite.background.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/background.ts'),
       name: 'Metalet',
+      formats: ['es'],
     },
     rollupOptions: {
       output: {
