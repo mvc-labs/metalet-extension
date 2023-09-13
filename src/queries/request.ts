d4f0514766550834c69528eabb03185ac8a028cb
AricRedemption <aricredemption@gmail.com>
2023-09-13 20:04:35 +0800

    chore: update fetch request function

--- src/queries/request.ts
+++ src/queries/request.ts
@@ -1,15 +1,50 @@
-import { METASV_TESTNET_HOST, METASV_HOST, METALET_HOST } from '../data/hosts'
+import { METASV_TESTNET_HOST, METASV_HOST, METALET_HOST, API2_ORDERS_EXCHANGE } from '../data/hosts'
 import { network } from '../lib/network'
 
-async function request(url: string, options = {}) {
-  const defaultOptions = {
-    headers: {
-      'Content-Type': 'application/json',
-    },
+const SIGNATURE_MESSAGE = 'metalet.space'
+
+type OptionParams = Record<string, string>
+
+interface OptionData {
+  [key: string]: unknown
+}
+
+interface requestOption {
+  method: 'GET' | 'POST'
+  data?: OptionData
+  params?: OptionParams
+  headers?: Headers
+  body?: string | URLSearchParams
+}
+
+async function request(url: string, options: requestOption) {
+  // const headers = new Headers({
+  //   'X-Signature': '',
+  //   'X-Public-Key': '',
+  // })
+  if (!options?.headers) {
+    options.headers = new Headers()
   }
-  const allOptions = { ...defaultOptions, ...options }
+  if (options?.params) {
+    if (options.method === 'GET') {
+      const params = new URLSearchParams(options.params)
+      url = `${url}?${params.toString()}`
+    } else {
+      // UNUSED: Params will not be processed in POST request
+      options.body = new URLSearchParams(options.params)
+    }
+    delete options.params
+    options.headers.set('Content-Type', 'application/x-www-form-urlencoded')
+  }
+
+  if (options?.data) {
+    options.body = JSON.stringify(options.data)
+    options.headers.set('Content-Type', 'application/json')
+    delete options.data
+  }
+
   return new Promise((resolve, reject) => {
-    fetch(url, allOptions)
+    fetch(url, options)
       .then((response) => {
         if (!response.ok) {
           throw new Error(response.statusText)
@@ -34,6 +69,13 @@ export const metaletApi = (path: string) => {
   return {
     get: (params?: any) => request(`${metaletHost}${path}`, { method: 'GET', params }),
     post: (data?: any) => request(`${metaletHost}${path}`, { method: 'POST', data }),
+  }
+}
+
+export const btcApi = (path: string) => {
+  return {
+    get: (params?: OptionParams) => request(`${API2_ORDERS_EXCHANGE}${path}`, { method: 'GET', params }),
+    post: (data?: OptionData) => request(`${API2_ORDERS_EXCHANGE}${path}`, { method: 'POST', data }),
   }
 }
 
