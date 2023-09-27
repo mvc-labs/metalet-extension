6af0f108ff52ee85390958bd621e3569198ee5d2
AricRedemption <aricredemption@gmail.com>
2023-09-28 01:18:30 +0800

    fix: BTC assets displayed incorrectly after switching addressType

--- src/lib/balance.ts
+++ src/lib/balance.ts
@@ -1,17 +1,21 @@
-import { computed, ref } from 'vue'
+import { computed, ref, watch } from 'vue'
+import { currentAccount } from './account'
 
 interface Asset {
   name: string
   value: number
 }
 
-export const totalBalance = computed(() => {
-  console.log('assetList', assetList.value)
-
-  return assetList.value.reduce((sum, asset) => sum + asset.value, 0)
-})
-
 export const assetList = ref<Asset[]>([])
+
+export const totalBalance = computed(() => assetList.value.reduce((sum, asset) => sum + asset.value, 0))
+
+watch(
+  () => currentAccount.value?.btc.addressType,
+  () => {
+    assetList.value = []
+  }
+)
 
 export async function updateAsset(asset: Asset) {
   const preAsset = assetList.value.find((assetItem) => assetItem.name === asset.name)
