701bf090456dff3d7d778f7b9116593d32a29e01
Vincent Tse <river_run@126.com>
2023-09-28 23:11:51 +0800

    🚀 VERSION: 2.0.1

--- src/lib/actions/connect.ts
+++ src/lib/actions/connect.ts
@@ -2,8 +2,6 @@ import { getAddress, getCurrentAccount } from '../account'
 import connector from '../connector'
 
 export async function process(params: any, host: string) {
-  console.log('connect process load')
-
   const account = await getCurrentAccount()
 
   if (!account) {
