a9cb4502d614a1ad4e174a5e0ca2feda0636df67
AricRedemption <aricredemption@gmail.com>
2024-01-15 13:03:21 +0800

    chore: Update btc connect api

--- src/lib/actions/btc/connect.ts
+++ src/lib/actions/btc/connect.ts
@@ -1,11 +1,17 @@
-import { getPublicKey, getAddress } from '@/lib/account'
+import connector from '../../connector'
+import { getPublicKey, getAddress, getCurrentAccount } from '@/lib/account'
 
 interface AccountInfo {
   address: string;
   pubKey: string;
 }
 
-export async function process(): Promise<AccountInfo> {
+export async function process(params: any, host: string): Promise<AccountInfo> {
+  const account = await getCurrentAccount()
+  if (!account) {
+    return { address: '', pubKey: '' }
+  }
+  await connector.connect(account.id, host)
   const address = await getAddress('btc')
   const pubKey = await getPublicKey('btc')
   return { address, pubKey }
