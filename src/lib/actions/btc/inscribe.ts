f68abf762a044f126bc54b077406ca947ada6381
Vincent Tse <river_run@126.com>
2024-02-22 17:13:45 +0800

    Update inscribe.ts

--- src/lib/actions/btc/inscribe.ts
+++ src/lib/actions/btc/inscribe.ts
@@ -428,7 +428,7 @@ export async function process({
 }): Promise<InscribeHexResult | InscribeTxIdResult> {
   const network = await getBtcNetwork()
   const address = await getAddress('btc')
-  const utxos = await getBtcUtxos(address)
+  const utxos = await getBtcUtxos(address).then((utxos) => utxos.filter((utxo) => utxo.confirmed))
   const commitTxPrevOutputList = utxos.map((utxo) => ({
     txId: utxo.txId,
     vOut: utxo.outputIndex,
