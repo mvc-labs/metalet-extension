4cd0c1e0cdd266f1fffe85676d35aa484dc7d236
AricRedemption <aricredemption@gmail.com>
2024-03-14 18:25:43 +0800

    chore: Update open wallet way

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,13 +3,21 @@ import { IS_DEV } from '@/data/config'
 import { twMerge } from 'tailwind-merge'
 import { type ClassValue, clsx } from 'clsx'
 import { camelize, getCurrentInstance, toHandlerKey } from 'vue'
+import { NOTIFICATION_HEIGHT, NOTIFICATION_WIDTH } from '@/data/config'
 
 export const gotoWelcome = (path: string) => {
   if (IS_DEV) {
     router.push('/welcome')
   } else {
-    const { origin, pathname } = window.location
-    window.open(`${origin}${pathname}/#${path}`, '_blank')
+    const browser = window.browser
+    browser.windows.create({
+      url: browser.runtime.getURL('popup.html#/welcome'),
+      type: 'popup',
+      width: NOTIFICATION_WIDTH,
+      height: NOTIFICATION_HEIGHT,
+      top: 0,
+      left: 0,
+    })
   }
 }
 
