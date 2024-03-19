a901cedd4408cfff9ae4574bd714c6c9fa848435
AricRedemption <aricredemption@gmail.com>
2024-03-19 15:36:28 +0800

    fix: Correct window mode page size on open

--- src/data/config.ts
+++ src/data/config.ts
@@ -4,8 +4,10 @@ export const FEEB = 1
 export const P2PKH_UNLOCK_SIZE = 1 + 1 + 72 + 1 + 33
 export const DERIVE_MAX_DEPTH = 1000
 
-export const NOTIFICATION_WIDTH = 360
-export const NOTIFICATION_HEIGHT = 600
+const IS_WINDOWS = /windows/i.test(navigator.userAgent)
+
+export const NOTIFICATION_WIDTH = IS_WINDOWS ? 366 : 360
+export const NOTIFICATION_HEIGHT = IS_WINDOWS ? 640 : 600
 
 export const DEBUG = false
 export const IS_DEV = process.env.NODE_ENV === 'development'
