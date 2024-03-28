ea7a35933c7cd1db19a3ea67c88070703e421747
AricRedemption <aricredemption@gmail.com>
2024-03-28 11:10:53 +0800

    fix: Resolved issue when setting custom fee rate

--- src/pages/wallet/components/BTCRateList.vue
+++ src/pages/wallet/components/BTCRateList.vue
@@ -44,7 +44,7 @@ watch(
         class="flex flex-col items-center justify-center rounded-md border cursor-pointer aspect-square"
       >
         <div class="text-xs">{{ rate.title }}</div>
-        <div class="mt-1.5 text-sm ">{{ rate.feeRate }} sat/vB</div>
+        <div class="mt-1.5 text-sm">{{ rate.feeRate }} sat/vB</div>
         <div class="mt-1 text-xs text-[#999999]">About</div>
         <div class="text-xs text-[#999999]">{{ rate.desc.replace('About', '') }}</div>
       </div>
@@ -63,7 +63,7 @@ watch(
       v-if="isCustom"
       placeholder="sat/vB"
       class="main-input w-full rounded-xl p-4 text-xs mt-4"
-      @input="(e) => emit('update:currentRateFee', (e.currentTarget as HTMLInputElement).value)"
+      @input="(e) => emit('update:currentRateFee', Number((e.currentTarget as HTMLInputElement).value))"
     />
   </div>
 </template>
