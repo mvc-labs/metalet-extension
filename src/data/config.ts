8b3e27f4a4b53cf8e8fd2c7ee32c89d144ed2ea8
AricRedemption <aricredemption@gmail.com>
2024-03-29 09:38:37 +0800

    fix(auth page): Corrected height display issue on auth page

--- src/data/config.ts
+++ src/data/config.ts
@@ -7,7 +7,7 @@ export const DERIVE_MAX_DEPTH = 1000
 const IS_WINDOWS = /windows/i.test(navigator.userAgent)
 
 export const NOTIFICATION_WIDTH = IS_WINDOWS ? 366 : 360
-export const NOTIFICATION_HEIGHT = IS_WINDOWS ? 640 : 600
+export const NOTIFICATION_HEIGHT = IS_WINDOWS ? 640 : 630
 
 export const DEBUG = false
 export const IS_DEV = process.env.NODE_ENV === 'development'
